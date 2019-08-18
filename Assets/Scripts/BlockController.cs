45c8c643c855cf27d402b0534740d9aedffc4222
Sarah Reszow <sreszow@gmail.com>
2019-08-19 00:26:25 +0200

    Particle Effects and different block types added

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -1,37 +1,94 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class BlockController : MonoBehaviour
 {
     #region Variables
     [SerializeField] AudioClip destroyClip;
+    [SerializeField] GameObject destroyEffect;
+    [SerializeField] Sprite[] hitSprites;
+
     LevelControl level;
     GameStatus game;
+
+    //states
+    int maxHit;
+    int timesHit = 0;
     #endregion
 
     #region Main Methods
     private void Start()
     {
+        maxHit = hitSprites.Length + 1;
         game = FindObjectOfType<GameStatus>();
-        level = FindObjectOfType<LevelControl>();
-        level.RegisterBlock();
+        CountBlock();
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+        
     }
     #endregion
 
     #region Helper Methods
-    private void DestroyBlock()
+    private void CountBlock()
+    {
+        if (tag == "Breakable")
+        {
+            level = FindObjectOfType<LevelControl>();
+            level.RegisterBlock();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if(timesHit >= maxHit)
+        {
+            BreakBlock();
+        }
+        else
+        {
+            DamageBlock();
+        }
+    }
+
+    private void DamageBlock()
+    {
+        if(timesHit <= hitSprites.Length)
+        {
+            int spriteIndex = timesHit - 1;
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite missing from " + gameObject.name);
+        }
+
+        TriggerDestroyVFX();
+    }
+
+    private void BreakBlock()
     {
         game.AddToScore();
-
         AudioSource.PlayClipAtPoint(destroyClip, Camera.main.transform.position);
         level.BreakBlock();
         Destroy(gameObject);
+        TriggerDestroyVFX();
+    }
+
+    private void TriggerDestroyVFX()
+    {
+        GameObject destroyBurst = Instantiate(destroyEffect, transform.position, transform.rotation);
+        Destroy(destroyBurst, 1f);
     }
     #endregion
 }
